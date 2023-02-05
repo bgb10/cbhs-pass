e9448760a92224edbbfd21774b357b3a9598a8fa
bgb10 <bgb10@naver.com>
2023-02-05 16:58:35 +0900

    4분 30초마다 qr 코드 갱신하도록 설정

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
-import React from 'react'
-import { SafeAreaView, StatusBar } from 'react-native'
+import React, { useRef, useState, useEffect, Component } from 'react'
+import { AppState, SafeAreaView, StatusBar } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 import { WebView } from 'react-native-webview'
 
-export default function App() {
-  const runFirst = `
-      document.getElementById('USER_ID').value = '202051'
-      document.getElementById('EMP_PW').value = '001028'
-      document.getElementById('SAVE_PW').checked = true
-      document.getElementById('LoginForm').submit()
-      true; // note: this is required, or you'll sometimes get silent failures
-    `
+const firstRun = `
+  document.getElementById('USER_ID').value = '202051'
+  document.getElementById('EMP_PW').value = '001028'
+  document.getElementById('SAVE_PW').checked = true
+  document.getElementById('LoginForm').submit()
+`
 
-  return (
-    <View
-      style={{
-        flex: 1
-      }}
-    >
-      <WebView
-        originWhitelist={['*']}
-        source={{ uri: 'http://115.92.96.29:8080/employee/login.jsp' }}
-        onMessage={(event) => {}}
-        injectedJavaScript={runFirst}
-      />
-    </View>
-  )
+const run = `
+if(window.location.href.includes('myInfo')) {
+  window.location.href = 'http://115.92.96.29:8080/employee/login.jsp'
 }
+
+  ${firstRun}
+  
+  true; // note: this is required, or you'll sometimes get silent failures
+`
+
+class App extends Component {
+  render() {
+    setInterval(() => {
+      this.webref.injectJavaScript(run)
+    }, 45000)
+
+    return (
+      <View
+        style={{
+          flex: 1
+        }}
+      >
+        <WebView
+          originWhitelist={['*']}
+          source={{ uri: 'http://115.92.96.29:8080/employee/login.jsp' }}
+          ref={(r) => (this.webref = r)}
+          injectedJavaScript={firstRun}
+        />
+      </View>
+    )
+  }
+}
+
+export default App
