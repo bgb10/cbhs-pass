2d6f1c763024cbcb225b2d2f1f2276c41f854353
bgb10 <bgb10@naver.com>
2023-02-05 15:45:47 +0900

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { SafeAreaView, StatusBar } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const runFirst = `
+      document.getElementById('USER_ID').value = '202051'
+      document.getElementById('EMP_PW').value = '001028'
+      document.getElementById('SAVE_PW').checked = true
+      document.getElementById('LoginForm').submit()
+      true; // note: this is required, or you'll sometimes get silent failures
+    `
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View
+      style={{
+        flex: 1
+      }}
+    >
+      <WebView
+        originWhitelist={['*']}
+        source={{ uri: 'http://115.92.96.29:8080/employee/login.jsp' }}
+        onMessage={(event) => {}}
+        injectedJavaScript={runFirst}
+      />
+    </View>
+  )
+}
