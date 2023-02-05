4449b2e38f9302f9a2701a9dcf413fbbf5d4b0cc
bgb10 <bgb10@naver.com>
2023-02-05 20:19:09 +0900

    최초 로그인이 성공한 후에만 자동 로그인 및 갱신이 되도록 개선

--- App.js
+++ App.js
@@ -3,44 +3,47 @@ import { AppState, SafeAreaView, StatusBar } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 import { WebView } from 'react-native-webview'
 
-const firstRun = `
-  document.getElementById('USER_ID').value = '202051'
-  document.getElementById('EMP_PW').value = '001028'
-  document.getElementById('SAVE_PW').checked = true
-  document.getElementById('LoginForm').submit()
-`
+const App = () => {
+  const [isLoginSuccess, setIsLoginSuccess] = useState(false)
 
-const run = `
-if(window.location.href.includes('myInfo')) {
-  window.location.href = 'http://115.92.96.29:8080/employee/login.jsp'
-}
+  const run = `
+  // 한 번이라도 로그인을 성공하면 자동 로그인이 되고, 4분 30초마다 로그인이 갱신된다.
+  if(window.location.href.includes('myInfo')) {
+    window.ReactNativeWebView.postMessage('loginSuccess');
 
-  ${firstRun}
+    setTimeout(() => {window.location.href = 'http://115.92.96.29:8080/employee/login.jsp'}, 10000)
+  }
+
+  // 로그인을 성공했을 때에만 자동 로그인이 시도된다.
+  if(${isLoginSuccess}) {
+    document.getElementById('USER_ID').value = '202051'
+    document.getElementById('EMP_PW').value = '001028'
+    document.getElementById('SAVE_PW').checked = true
+
+    document.getElementById('LoginForm').submit()
+  }
   
   true; // note: this is required, or you'll sometimes get silent failures
 `
 
-class App extends Component {
-  render() {
-    setInterval(() => {
-      this.webref.injectJavaScript(run)
-    }, 45000)
-
-    return (
-      <View
-        style={{
-          flex: 1
-        }}
-      >
-        <WebView
-          originWhitelist={['*']}
-          source={{ uri: 'http://115.92.96.29:8080/employee/login.jsp' }}
-          ref={(r) => (this.webref = r)}
-          injectedJavaScript={firstRun}
-        />
-      </View>
-    )
+  const handleEvent = (e) => {
+    setIsLoginSuccess(true)
   }
+
+  return (
+    <View
+      style={{
+        flex: 1
+      }}
+    >
+      <WebView
+        originWhitelist={['*']}
+        source={{ uri: 'http://115.92.96.29:8080/employee/login.jsp' }}
+        injectedJavaScript={run}
+        onMessage={handleEvent}
+      />
+    </View>
+  )
 }
 
 export default App
